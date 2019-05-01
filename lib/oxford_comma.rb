d3e9a3a6de924d89556ffe50ff46167321c16888
Kevin Dai <daikevin19@yahoo.com>
2019-05-01 16:33:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array.join
+  elsif array.size == 2 
+    return array.join(" and ")
+  elsif array.size >= 3
+    last_array_item = "and #{array[-1]}"
+    array.pop
+    array.push(last_array_item)
+    return array.join(", ")
+  end
 end
