948048e82456dc91daf9ebfb492befaa8e1d7105
Kevin Dai <daikevin19@yahoo.com>
2019-05-01 16:33:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,9 @@ def oxford_comma(array)
   elsif array.size == 2 
     return array.join(" and ")
   elsif array.size >= 3
-    last_array_item = "and #{array[-1]}"
-    array.pop
-    array.push(last_array_item)
-    return array.join(", ")
+    last_array_item = "and #{array[-1]}" ##separate last item "and"
+    array.pop ##remove previous last item
+    array.push(last_array_item) ##push in new item
+    return array.join(", ") ##join everything together
   end
 end
